b92edfce675f3a442e15fdd589ed1eca218a1543
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2024-10-21 16:07:12 +0000

    initial commit

--- debugger.rb
+++
@@ -1,9 +0,0 @@
-require "pry-byebug"
-
-f = "Your lucky number is "
-
-l = rand(100)
-
-byebug
-
-pp f + l
--- howdy.rb
+++ howdy.rb
@@ -1,12 +1,6 @@
-require "active_support/all"
 
-puts "What's your name?"
+pp "What's your name?"
 
-their_name = gets.chomp
+your_name  = gets.chomp
 
-pp "Hello, " + their_name + "!"
-
-my_string = "Hello, world!"
-pp my_string
-
-require "./goodbye.rb"
+puts "Hello, " + your_name + "!"
